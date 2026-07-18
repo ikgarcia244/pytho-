ac7a49d3eb8c12d7ceb833927054a71f8711586d
ikgarcia244 <ikgarciagarcia@gmail.com>
2026-07-18 14:20:05 -0600

    Add Discord bot with multiple commands
    Implemented a Discord bot with various commands including greetings, coin flip, member join date, choice selection, message repetition, and a custom 'heh' command.

---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import random 
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def moneda(ctx):
+    numero = random.randint(1,2)
+    if numero == 1:
+        await ctx.send("Cara")
+    elif numero == 2:
+        await ctx.send("sello")
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("")
